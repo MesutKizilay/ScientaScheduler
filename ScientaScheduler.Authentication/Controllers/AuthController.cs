020572c4435ce55e92e20ccbb01afea642ade882
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-03 15:55:39 +0300

    JWT konfigürasyonu eklendi

---
+++ ScientaScheduler.Authentication/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ScientaScheduler.Authentication.JWT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScientaScheduler.Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }        
+        
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult SignIn()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ScientaScheduler.Authentication/JWT/BuildToken.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScientaScheduler.Authentication.JWT
+{
+    public class BuildToken
+    {
+        public string CreateToken()
+        {
+            var bytes = Encoding.UTF8.GetBytes("ScientaSchedulerAuthentication");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: "http://localhost",
+                audience: "http://localhost",
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials:signingCredentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}

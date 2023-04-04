8f9a72d39ecbcd30522962149ef9cdf7819cf5b4
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-04 09:32:35 +0300

    .

--- ScientaScheduler.Authentication/Authentication/BuildToken.cs
+++ ScientaScheduler.Authentication/Authentication/BuildToken.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -10,17 +11,27 @@ namespace ScientaScheduler.Authentication.JWT
 {
     public class BuildToken
     {
+        IConfiguration Configuration
+        public BuildToken(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public string CreateToken()
         {
-            var bytes = Encoding.UTF8.GetBytes("ScientaSchedulerAuthentication");
+            var issuer = Configuration["JwtConfig:Issuer"];
+            var audience = Configuration["JwtConfig:Audience"];
+            var signingKey = Configuration["JwtConfig:SigningKey"];
+            var bytes = Encoding.UTF8.GetBytes(signingKey);
+
             SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
             SigningCredentials signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             JwtSecurityToken token = new JwtSecurityToken(
-                issuer: "http://localhost",
-                audience: "http://localhost",
+                issuer: issuer,
+                audience: audience,
                 notBefore: DateTime.Now,
                 expires: DateTime.Now.AddMinutes(1),
-                signingCredentials:signingCredentials);
+                signingCredentials: signingCredentials);
 
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(token);

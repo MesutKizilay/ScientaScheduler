e00e47782d4bf15e6a683c5f344945f55fe6c0c7
Giray VARDAL <gvardal@yahoo.com>
2023-04-04 16:19:23 +0300

    Implement ScientaScheduler.Application

---
+++ ScientaSchedurler.Application/Infrastucture/IProject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScientaSchedurler.Application.Infrastucture
+{
+    public interface IProject
+    {
+        void GetProjectList();
+
+        void GetProjectById();
+
+        void UpdateProject();
+
+        void DeleteProject();
+    }
+}
---
+++ ScientaSchedurler.Application/Interfaces/ProjectHandler.cs
@@ -0,0 +1,27 @@
+﻿using ScientaSchedurler.Application.Infrastucture;
+
+namespace ScientaSchedurler.Application.Interfaces
+{
+    public class ProjectHandler : IProject
+    {
+        public void DeleteProject()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetProjectById()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetProjectList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateProject()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

8f1fbe9daf614ee7831e96a604b468289f710e32
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-04 08:10:07 +0300

    .

---
+++ ScientaScheduler.Business/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScientaScheduler.Business.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+       
+        [HttpGet("[action]")]
+        public IActionResult SignIn()
+        {
+            return Ok("Başarıyla giriş yapıldı");
+        }
+    }
+}
---
+++ ScientaScheduler.Business/Controllers/JwtController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ScientaScheduler.Authentication.JWT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScientaScheduler.Business.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+    }
+}

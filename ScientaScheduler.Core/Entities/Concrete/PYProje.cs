fed694b873d530d49d2c77ec07d361bea29d4b32
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-04 15:19:30 +0300

    Core katmanında düzenlemeler yapıldı, Proje entity'si oluşturuldu.

---
+++ ScientaScheduler.Core/Entities/Concrete/PYProje.cs
@@ -0,0 +1,19 @@
+﻿using ScientaScheduler.Core.Entities.Abstarct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScientaScheduler.Core.Entities.Concrete
+{
+    public class PYProje:IEntity
+    {
+        public int ID { get; set; }
+        public int ProjeKodu { get; set; }
+        public string ProjectAdi { get; set; }
+        public DateTime PlanlananBaslamaTarihi { get; set; }
+        public DateTime PlanlananBitisTarihi { get; set; }
+        public int IsinSuresi { get; set; }
+    }
+}

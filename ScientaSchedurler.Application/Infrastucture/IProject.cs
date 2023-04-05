d52e44309cbc578c6fe4a481d9d4fc42ce1c6b98
Giray VARDAL <gvardal@yahoo.com>
2023-04-05 08:41:08 +0300

    Add Todos Item

--- ScientaSchedurler.Application/Infrastucture/IProject.cs
+++ ScientaSchedurler.Application/Infrastucture/IProject.cs
@@ -6,12 +6,9 @@ namespace ScientaSchedurler.Application.Infrastucture
     public interface IProject
     {
         List<PYProje> GetProjectList();
-
         PYProje GetProjectById(int id);
-
         void UpdateProject(PYProje pYProje);
-
         void DeleteProject(PYProje pYProje);
-        void AddProject(PYProje pYProje)
+        void AddProject(PYProje pYProje);
     }
 }

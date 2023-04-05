119eb591d74e772111d48c9b9836c0ace3eb73e6
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-05 11:39:43 +0300

    Syncfusion için lisans anahtarı eklendi

--- ScientaScheduler.BlazorApp/Startup.cs
+++ ScientaScheduler.BlazorApp/Startup.cs
@@ -35,6 +35,10 @@ namespace ScientaScheduler.BlazorApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense
+               ("Mgo+DSMBaFt+QHFqVkFrXVNbdV5dVGpAd0N3RGlcdlR1fUUmHVdTRHRcQl5gSX5Rck1jWXhbdHQ=;Mgo+DSMBPh8sVXJ1S0d+X1ZPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSX1QdkVhXXdecHNTQGA=;ORg4AjUWIQA/Gnt2VFhhQlJDfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5QdUVjW3tWcHRSQWlc;MTU1MzA5NEAzMjMxMmUzMTJlMzMzN0pxN0RJdHo0ZnFRMnBjZnczOExPM2R5TVRjUkxJcHBqQllGb2VJRWwwalE9;MTU1MzA5NUAzMjMxMmUzMTJlMzMzN0FBelpnSmg3Y3BDOG9zZ21SNGdHVVc5ZXNUSEE0cURCNVlyK0JlbXpEOUk9;NRAiBiAaIQQuGjN/V0d+XU9HcVRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS31TdUZjWX1aeHRVTmRfUQ==;MTU1MzA5N0AzMjMxMmUzMTJlMzMzN2J5ekZQZFVJandZN1hJOUJPUGNwbTd6a3FhOGNMeTdxREpNZXJXd2oySjg9;MTU1MzA5OEAzMjMxMmUzMTJlMzMzN002SnFXdnNZNzZEZFdSeUVFQ0NpUFpvQ09ocDJmbjAzRVYyWkFxMDBqOE09;Mgo+DSMBMAY9C3t2VFhhQlJDfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5QdUVjW3tWcHRcRmdc;MTU1MzEwMEAzMjMxMmUzMTJlMzMzN0kyWmlsUi85MEROWTgrc01zY2s3eHFVaHZOdFVZVU1ZOXpnN1lEVW1oMkU9;MTU1MzEwMUAzMjMxMmUzMTJlMzMzN0IxVVcvdlBldHJTUCt4U0pMR0ZkRmdNTUN3blpITm9mNFNxeksvS2hUVTA9;MTU1MzEwMkAzMjMxMmUzMTJlMzMzN2J5ekZQZFVJandZN1hJOUJPUGNwbTd6a3FhOGNMeTdxREpNZXJXd2oySjg9");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

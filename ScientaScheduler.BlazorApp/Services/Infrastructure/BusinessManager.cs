7ea6d5aa884678c23cb9b0eb26ff5530012ba42d
Giray VARDAL <gvardal@yahoo.com>
2023-04-06 13:53:52 +0300

    Açıklamalar silindi

--- ScientaScheduler.BlazorApp/Services/Infrastructure/BusinessManager.cs
+++ ScientaScheduler.BlazorApp/Services/Infrastructure/BusinessManager.cs
@@ -24,7 +24,6 @@ namespace ScientaScheduler.BlazorApp.Services.Infrastructure
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        // Göstermek için bir fonksiyon yazıyorum.
         public async Task<ProjectDto> GetProjeById()
         {
             ProjectDto proje = new();
@@ -32,22 +31,7 @@ namespace ScientaScheduler.BlazorApp.Services.Infrastructure
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //Eğer business katmanında oluşturduğumuz controller [Authorization] ile korunuyor ise
-            //kendisine token vermemiz gerekecektir. tabi burada elimizde bir token olması gerekir.
-            //şimdilik controller [Auth] ile korunmadığından bunu yorum satırı haline alıyorum
-            //httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-
-            // Burada artık isteklerini gönderiyorsun
-            // hangi adrese yukarıda belirttiğin baseAdress e
-            // ama biz dapper yapmıyacağız şimdilik base i kullanacağız
-
-            // şimdi buraya business da bulunan getbyıd adresine bir get isteği oluşturup onu da bir listeye aktarır mısın
-
-
             using HttpResponseMessage response = await httpClient.GetAsync("/Project/GetProjectById?id=1");
-            // onun için gelen nesneyi çevirmemiz gerekecek
-            // ama ondan önce bir satır daha yazmam lazım gelen bir konu daha var
             if (response.IsSuccessStatusCode)
             {
                 var contentString = await response.Content.ReadAsStringAsync();
